f7ce21b924cce1f340af4b137e1ebf7d155fa875
gulamchy <gulamsulaman@gmail.com>
2024-10-21 17:50:21 -0700

    Updating image files path with vite1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
-      '@': resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets')
     },
   },
 })
